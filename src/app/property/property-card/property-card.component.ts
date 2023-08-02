f6319fc88c5bbc30a2f7248f6d0a8036bf555f0a
Altana <altanagrishkina@gmail.com>
2023-08-02 13:26:45 +0200

    Passing data beetwen components_Using ngFor Directive_Using property binding for Create property list

--- src/app/property/property-card/property-card.component.ts
+++ src/app/property/property-card/property-card.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, Input } from "@angular/core";
 
 
 @Component({
@@ -9,11 +9,6 @@ import { Component } from "@angular/core";
   styleUrls: ['property-card.component.css']
 })
 export class PropertyCardComponent {
+@Input() property : any
 
-  Property: any = {
-    "Id": 1,
-    "Name": "Birla House",
-    "Type": "House",
-    "Price": 12000
-  }
 }
