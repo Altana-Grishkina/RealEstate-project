15519a48a9c0a59bdca0a00ab96a1d45ebbe964f
Altana <altanagrishkina@gmail.com>
2023-08-08 10:48:34 +0200

    Using Service and HTTP calls in Angular 9

--- src/app/property/property-list/property-list.component.ts
+++ src/app/property/property-list/property-list.component.ts
@@ -1,48 +1,33 @@
-import { Component } from '@angular/core';
+
+import { Component, OnInit } from '@angular/core';
+import { HousingService } from 'src/app/services/housing.service';
 
 @Component({
   selector: 'app-property-list',
   templateUrl: './property-list.component.html',
   styleUrls: ['./property-list.component.css']
 })
-export class PropertyListComponent {
+export class PropertyListComponent implements OnInit {
 
-  properties: Array<any> = [
-    {
-      "Id": 1,
-      "Name": "Birla House",
-      "Type": "House",
-      "Price": 12000
-    },
-    {
-      "Id": 2,
-      "Name": "Erose Villa",
-      "Type": "Villa",
-      "Price": 12500
-    },
-    {
-      "Id": 3,
-      "Name": "Mark Hill",
-      "Type": "Flat",
-      "Price": 12000
-    },
-    {
-      "Id": 4,
-      "Name": "Gun Hill",
-      "Type": "Duplex",
-      "Price": 11400
-    },
-    {
-      "Id": 5,
-      "Name": "Macro House",
-      "Type": "House",
-      "Price": 14300
-    },
-    {
-      "Id": 6,
-      "Name": "Pearl White House",
-      "Type": "House",
-      "Price": 18000
-    }
-]
+  properties: any;
+
+  constructor(private housingService: HousingService){}
+
+  ngOnInit(): void {
+    this.housingService.getAllProperties().subscribe(
+      data=>{
+          this.properties=data;
+          console.log(data);
+      }, errore => {
+        console.log(errore);
+      }
+    )
+    // this.http.get('data/properties.json').subscribe(
+    // data=>{
+    //   this.properties=data;
+    //   console.log(data);
+    // }
+
+    // );
+  }
 }
