37773b7dbf71398bcc908cd81103a87e81e361b7
Altana <altanagrishkina@gmail.com>
2023-08-08 11:28:00 +0200

    Modify HTTP Data using Pipe

--- src/app/property/property-card/property-card.component.ts
+++ src/app/property/property-card/property-card.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from "@angular/core";
+import { IProperty } from "../property-list/iProperty.interface";
 
 
 @Component({
@@ -9,6 +10,7 @@ import { Component, Input } from "@angular/core";
   styleUrls: ['property-card.component.css']
 })
 export class PropertyCardComponent {
-@Input() property : any
+  @Input()
+  property!: IProperty;
 
 }
