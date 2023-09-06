ff1932524d2cd3f2c7831e90ef8a9faaf67b0a59
Altana <altanagrishkina@gmail.com>
2023-09-06 18:29:19 +0200

    Reactive Forms in angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { PropertyCardComponent } from './property/property-card/property-card.component';
 import { PropertyListComponent } from './property/property-list/property-list.component';
@@ -10,12 +10,17 @@ import { HousingService } from './services/housing.service';
 import { AddPropertyComponent } from './property/add-property/add-property.component';
 import {Routes, RouterModule} from '@angular/router';
 import { PropertyDetailComponent } from './property/property-detail/property-detail.component';
+import { UserRegisterComponent } from './user/user-register/user-register.component';
+import { UserLoginComponent } from './user/user-login/user-login.component';
+
 
 const appRoutes: Routes = [
   { path: '', component: PropertyListComponent},
   { path: 'rent-property', component: PropertyListComponent},
   { path: 'add-property', component: AddPropertyComponent},
   { path: 'property-detail/:id', component:  PropertyDetailComponent},
+  { path: 'user/login', component: UserLoginComponent},
+  { path: 'user/register', component: UserRegisterComponent},
   { path: '**', component: PropertyListComponent}
 ]
 
@@ -26,12 +31,15 @@ const appRoutes: Routes = [
     PropertyListComponent,
     NavBarComponent,
     AddPropertyComponent,
-    PropertyDetailComponent
-  ],
+    PropertyDetailComponent,
+    UserRegisterComponent,
+    UserLoginComponent
+   ],
   imports: [
     BrowserModule,
     HttpClientModule,
     FormsModule,
+    ReactiveFormsModule,
     RouterModule.forRoot(appRoutes)
   ],
   providers: [HousingService],
